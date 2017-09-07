2b3159170360adcb091b6462229b10e78551ca8b
patmalt <patmalt@gmail.com>
2017-09-07 12:42:27 -0500

    Initial commit

---
+++ TransparentNavigationBarTransitions/TransparentNavigationBarTransitions/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TransparentNavigationBarTransitions
+//
+//  Created by Patrick Maltagliati on 9/7/17.
+//  Copyright © 2017 Patrick Maltagliati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func loadView() {
+        let view = UIView()
+        view.backgroundColor = .lightGray
+        self.view = view
+    }
+}
