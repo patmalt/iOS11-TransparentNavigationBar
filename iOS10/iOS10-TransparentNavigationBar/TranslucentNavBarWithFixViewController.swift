5c5924a4d9278e5a5ad3595ab57f2f71abf15003
wes.ehrlichman <wes.ehrlichman@asynchrony.com>
2017-09-10 20:52:56 -0500

    Split into an iOS 10 and iOS 11 version - Wes

---
+++ iOS10/iOS10-TransparentNavigationBar/TranslucentNavBarWithFixViewController.swift
@@ -0,0 +1,24 @@
+
+import UIKit
+
+class TranslucentNavBarWithFixViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		title = "10.0 Fix in 11.0"
+		view.backgroundColor = .lightGray
+	}
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+        extendedLayoutIncludesOpaqueBars = true
+        edgesForExtendedLayout = .top
+        navigationController?.navigationBar.isTranslucent = true
+		navigationController?.navigationBar.alpha = 1.0
+		navigationController?.navigationBar.barTintColor = .purple
+		navigationController?.navigationBar.tintColor = .white
+		navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName : UIColor.white]
+        navigationController?.navigationBar.subviews.first?.alpha = 0.2
+    }
+}
+
